0b0e33b2c4c9d26024c77ac4f75016937447ab44
boapi <tobias.weyer1205@gmail.com>
2022-06-27 22:23:35 +0200

    add normalize.css to output

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 // eleventy config file
 module.exports = function (eleventyConfig) {
   // Copy the `css` folders to the output
-  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({
+    "src/assets": "assets",
+    "node_modules/normalize.css/normalize.css": "assets/normalize.css",
+  });
 };
