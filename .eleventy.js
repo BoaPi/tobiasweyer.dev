a4d47a77243cb3466235410395c8ffb225b7a10e
boapi <tobias.weyer1205@gmail.com>
2022-06-30 22:57:08 +0200

    adjust e11y config to copy sass output and watch sass output for rebuilds

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,10 @@
 module.exports = function (eleventyConfig) {
   // Copy the `css` folders to the output
   eleventyConfig.addPassthroughCopy({
-    "src/assets": "assets",
-    "node_modules/normalize.css/normalize.css": "assets/normalize.css",
+    "src/assets/main.css": "assets/main.css",
+    "src/assets/main.css.map": "assets/main.css.map",
   });
+
+  // watch new scss builds
+  eleventyConfig.addWatchTarget("src/assets/main.css");
 };
