0236a5ff005d48066d9dac97fad4e1df20a4bfd7
boapi <tobias.weyer1205@gmail.com>
2022-07-06 23:24:17 +0200

    update path to new stylesheet structure

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,10 @@
 module.exports = function (eleventyConfig) {
   // Copy the `css` folders to the output
   eleventyConfig.addPassthroughCopy({
-    "src/assets/main.css": "assets/main.css",
-    "src/assets/main.css.map": "assets/main.css.map",
+    "src/assets/styles/main.css": "assets/main.css",
+    "src/assets/styles/main.css.map": "assets/main.css.map",
   });
 
   // watch new scss builds
-  eleventyConfig.addWatchTarget("src/assets/main.css");
+  eleventyConfig.addWatchTarget("src/assets/styles/main.css");
 };
