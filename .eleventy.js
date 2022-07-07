e29f92efef0c1c2f547adc055340b81f974192ce
boapi <tobias.weyer1205@gmail.com>
2022-07-07 23:37:52 +0200

    turn off usage of .gitignore add ignore rules

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
 // eleventy config file
 module.exports = function (eleventyConfig) {
+  // ignored files, turn off opt-in of .gitignore
+  eleventyConfig.setUseGitIgnore(false);
+  eleventyConfig.ignores.add("README.md");
+  eleventyConfig.ignores.add("node_modules/**");
+
   // Copy the `css` folders to the output
   eleventyConfig.addPassthroughCopy({
     "src/assets/styles/main.css": "assets/main.css",
